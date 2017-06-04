cf66a557d72952dac9756e42cf14163049faee2f
Ihor <iwenchak@gmail.com>
2017-06-04 16:22:44 +0300

    test C#

---
+++ TagCleanerWin/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TagCleanerWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.tagsBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(36, 33);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(794, 193);
+            this.inputBox.TabIndex = 0;
+            // 
+            // tagsBox
+            // 
+            this.tagsBox.Location = new System.Drawing.Point(36, 262);
+            this.tagsBox.Multiline = true;
+            this.tagsBox.Name = "tagsBox";
+            this.tagsBox.Size = new System.Drawing.Size(793, 73);
+            this.tagsBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(36, 364);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(793, 139);
+            this.outputBox.TabIndex = 2;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(717, 529);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(111, 30);
+            this.goButton.TabIndex = 3;
+            this.goButton.Text = "Parsing";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter block:\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter allowed tags:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 570);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.tagsBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox tagsBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TagCleanerWin/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TagCleanerWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            inputBox.Text = "";
+            tagsBox.Text = "";
+        }
+
+        public Boolean Include(String sub, String[] substrings)
+        {
+            Char[] delimiters = { ' ', '=', '/', '<', '>' };
+            String[] allowed = sub.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            String elem = "";
+
+            if (allowed.Length > 0)
+                elem = allowed[0];
+
+            Boolean attr = true;
+
+
+            foreach (var substring in substrings)
+            {
+
+                if (elem.Equals(substring))
+                    attr = false;
+            }
+
+            return attr;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            String block = inputBox.Text.Trim();
+
+            String tags = tagsBox.Text.Trim();
+
+            Char[] delimiters = { ',', ';', ' ' };
+            String[] substrings = tags.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            int size = block.Length;
+
+            String str = "";
+            String new_str = "";
+
+            int _single = 0;
+            int _double = 0;
+            int count = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                new_str += block[i];
+
+                if ((block[i].Equals("'")) && (_single == 0))
+                    _single = 1;
+                else if ((block[i].Equals("'")) && (_single == 1))
+                    _single = 0;
+
+                if ((block[i].Equals('"')) && (_double == 0))
+                    _double = 1;
+                else if ((block[i].Equals('"')) && (_double == 1))
+                    _double = 0;
+
+
+                if (block[i].Equals('<') && (str.Length != 1) && (_single == 0) && (_double == 0))
+                {
+                    str += block[i];
+                    count += 1;
+                }
+                else if (block[i].Equals('<') && ((_single > 0) || (_double > 0)))
+                {
+                    str += block[i];
+
+                }
+
+                if (block[i].Equals('<') && (str.Length == 1) && (_single == 0) && (_double == 0))
+                {
+                    str = "<";
+                    count = 1;
+                }
+
+                if (block[i].Equals('>') && (str.Length == 1) && (_single == 0) && (_double == 0))
+                {
+                    str = "";
+                    count = 0;
+                }
+
+
+                if ((str.Length > 0) && (!block[i].Equals('<')))
+                {
+                    str += block[i];
+                }
+
+
+
+                if (block[i].Equals('>') && (count > 0) && (_single == 0) && (_double == 0))
+                {
+                    count -= 1;
+                }
+
+
+
+                if ((count == 0) && (str.Length > 2))
+                {
+
+                    if (Include(str, substrings))
+                    {
+
+                        new_str = new_str.Replace(str, "");
+
+                    }
+
+                    str = "";
+                }
+
+            }
+
+            outputBox.Text = new_str;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
